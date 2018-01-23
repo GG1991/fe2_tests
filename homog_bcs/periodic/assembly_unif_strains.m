bd234eab64cb989b8a20a85e87e35f79f81f5058
GG1991 <giuntoli1991@gmail.com>
2018-01-23 11:50:18 +0100

    starting unif_strains homog

---
+++ homog_bcs/periodic/assembly_unif_strains.m
@@ -0,0 +1,39 @@
+%  ensambla el Jacobiano (J) y el residuo (r)
+
+function [jac, res] = assembly_unif_strains (nx, ny, dx, dy)
+
+nnods = nx*ny;
+dim = 2;
+
+wg = [0.25 , 0.25, 0.25 , 0.25];
+
+xg = [ -0.577350269189626, -0.577350269189626;
+       +0.577350269189626, -0.577350269189626;
+       +0.577350269189626, +0.577350269189626;
+       -0.577350269189626, +0.577350269189626 ];
+
+dsh = zeros(npe, dim, npe);
+for gp = 1 : npe
+   dsh(1,1,gp) = -1 * (1 - xg(gp,2)) /4 * 2/dx;
+   dsh(2,1,gp) = +1 * (1 - xg(gp,2)) /4 * 2/dx;
+   dsh(3,1,gp) = +1 * (1 + xg(gp,2)) /4 * 2/dx;
+   dsh(4,1,gp) = -1 * (1 + xg(gp,2)) /4 * 2/dx;
+   dsh(1,2,gp) = -1 * (1 - xg(gp,1)) /4 * 2/dy;
+   dsh(2,2,gp) = -1 * (1 + xg(gp,1)) /4 * 2/dy;
+   dsh(3,2,gp) = +1 * (1 + xg(gp,1)) /4 * 2/dy;
+   dsh(4,2,gp) = +1 * (1 - xg(gp,1)) /4 * 2/dy;
+end
+
+jac = sparse(dim*nnods, dim*nnods);
+res = zeros(dim*nnods, 1);
+
+for e = 1 : size(elements, 1) 
+
+    [ejac, eres] = calc_elemental (e, xg, wg, dsh)
+
+    jac(elements(e,:), elements(e,:)) += ejac;
+    res(elements(e,:), 1) += eres;
+
+end
+
+endfunction
---
+++ homog_bcs/periodic/calc_elemental.m
@@ -0,0 +1,13 @@
+% fiber in the middle of a circle, structured mesh
+
+function [ejac, eres] = calc_elemental (e, dx, dy)
+
+dim = 2;
+npe = 4;
+
+ejac = zeros(dim*npe, dim*npe);
+eres = zeros(dim*npe, 1);
+
+
+
+endfunction
