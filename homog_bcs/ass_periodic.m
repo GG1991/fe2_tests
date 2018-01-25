a5367aa108ecc32689b4c4700b717d13524d66f1
GG1991 <giuntoli1991@gmail.com>
2018-01-25 14:36:52 +0100

    starting the periodic case

---
+++ homog_bcs/ass_periodic.m
@@ -0,0 +1,49 @@
+function [jac, res] = ass_periodic (strain_mac, u_n)
+
+global elements
+global coordinates
+global bc_nods
+global nnods
+global npe
+global dim
+global nelem
+
+u_e = zeros(npe*dim,1);
+jac = sparse(dim*nnods, dim*nnods);
+res = zeros(dim*nnods, 1);
+ind = zeros(npe*dim, 1);
+
+for e = 1 : nelem 
+
+    u_e([1:2:npe*dim]) = u_n([elements(e, :)*dim - 1]); %set x vals
+    u_e([2:2:npe*dim]) = u_n([elements(e, :)*dim + 0]); %set y vals
+
+    [jac_e, res_e] = elemental (e, u_e);
+    for n = 1 : npe 
+      for d = 0 : 1
+        ind(n*dim - d) = elements(e,n)*dim - d;
+      end
+    end
+
+    jac(ind, ind) += jac_e;
+    res([elements(e,:)*dim - 1]) += res_e([1:2:size(res_e,1)]); %set x vals
+    res([elements(e,:)*dim + 0]) += res_e([2:2:size(res_e,1)]); %set y vals
+
+end
+
+u_d = zeros(size(bc_nods, 1)*dim, 1);
+for n = 1 : size(bc_nods, 1)
+  u_d([n*dim - 1, n*dim]) = [strain_mac(1) strain_mac(3)/2 ; strain_mac(3)/2 strain_mac(2)] * coordinates(bc_nods(n), :)';
+end
+
+res([bc_nods*dim - 1]) = u_n([bc_nods*dim - 1]) - u_d([1:2:size(u_d,1)]); %set x vals
+res([bc_nods*dim + 0]) = u_n([bc_nods*dim + 0]) - u_d([2:2:size(u_d,1)]); %set y vals
+
+jac([bc_nods*dim - 1; bc_nods*dim], :) = 0.0;
+for n = 1 : size(bc_nods, 1)
+  for d = 0 : 1
+    jac(bc_nods(n)*dim - d, bc_nods(n)*dim - d) = 1.0;
+  end
+end
+
+endfunction
---
+++ homog_bcs/run_periodic.m
@@ -0,0 +1,70 @@
+global elements
+global coordinates
+global elem_type
+global bc_nods
+global bc_y0
+global bc_y1
+global bc_x0
+global bc_x1
+global xg
+global wg
+global b_mat
+global stress
+global strain
+
+global nx = 4;
+global ny = 4;
+global nelem = (nx-1)*(ny-1)
+global nnods = nx*ny;
+global lx = 3;
+global ly = 3;
+global dx = lx / (nx - 1);
+global dy = ly / (ny - 1);
+
+global npe = 4;
+global dim = 2;
+global nvoi = 3;
+
+init_vars();
+
+elem_type = zeros(nelem, 1);
+
+#elements
+#coordinates
+#bc_nods
+
+du = zeros(nx*ny*dim, 1);
+strain = zeros((nx-1)*(ny-1), nvoi);
+stress = zeros((nx-1)*(ny-1), nvoi);
+
+dir_n = zeros(nx*ny*dim, 1);
+strain_exp = [0.005 0 0; 0 0.005 0; 0 0 0.005]';
+
+c_ave = zeros(3,3);
+
+for i = 1 : 3
+
+u = zeros(nx*ny*dim, 1);
+printf ("\033[31mstrain = %f %f %f\n\033[0m", strain_exp(:,i)');
+
+[jac, res] = ass_unifstrains (strain_exp(:,i), u);
+printf ("\033[32m|res| = %f\n\033[0m", norm(res));
+
+du = -(jac\res);
+u = u + du;
+
+[jac, res] = ass_unifstrains (strain_exp(:,i), u);
+printf ("\033[32m|res| = %f\n\033[0m", norm(res));
+
+[strain_ave, stress_ave] = average();
+c_ave(:,i) = stress_ave' / strain_ave(i);
+
+end
+
+printf ("\n");
+c_ave
+
+%figure();
+%spy(jac); print -djpg spy.jpg 
+
+write_vtk("sol.vtk", u)
