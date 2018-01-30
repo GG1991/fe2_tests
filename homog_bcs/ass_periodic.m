7594fb201ea65a69f6559ea93bb602d80f56b247
GG1991 <giuntoli1991@gmail.com>
2018-01-30 10:42:56 +0100

    periodic with lsgrange multipliers works but do not give the correct result, shoud check

--- homog_bcs/ass_periodic.m
+++ homog_bcs/ass_periodic.m
@@ -2,6 +2,8 @@ function [jac, res] = ass_periodic (strain_mac, u_n)
 
 global elements
 global coordinates
+global stress
+global strain
 global bc_nods
 global bc_y0
 global bc_y1
@@ -9,16 +11,20 @@ global bc_x0
 global bc_x1
 global lx
 global ly
+global dx
+global dy
 global nx
 global ny
 global nnods
 global npe
 global dim
 global nelem
+global size_tot
 
-u_e = zeros(npe*dim,1);
-jac = sparse(dim*nnods, dim*nnods);
-res = zeros(dim*nnods, 1);
+
+jac = sparse(size_tot, size_tot);
+res = zeros(size_tot, 1);
+u_e = zeros(npe*dim, 1);
 ind = zeros(npe*dim, 1);
 
 for e = 1 : nelem 
@@ -38,38 +44,50 @@ for e = 1 : nelem
 
 end
 
-mid_nod = nx*(ny/2);
+X0Y0_nod = 1;
 
 u_dif_y0 = [strain_mac(1) strain_mac(3)/2 ; strain_mac(3)/2 strain_mac(2)] * [0.0, ly]';
 u_dif_x0 = [strain_mac(1) strain_mac(3)/2 ; strain_mac(3)/2 strain_mac(2)] * [lx , 0.0]';
 
-res([bc_y0*dim - 1]) = u_n([bc_y1*dim - 1]) - u_n([bc_y0*dim - 1]) - u_dif_y0(1); %set x vals
-res([bc_y0*dim - 0]) = u_n([bc_y1*dim - 0]) - u_n([bc_y0*dim - 0]) - u_dif_y0(2); %set y vals
-res([bc_x0*dim - 1]) = u_n([bc_x1*dim - 1]) - u_n([bc_x0*dim - 1]) - u_dif_x0(1); %set x vals
-res([bc_x0*dim - 0]) = u_n([bc_x1*dim - 0]) - u_n([bc_x0*dim - 0]) - u_dif_x0(2); %set y vals
+res([1:2:max(size(bc_y0))*dim] + nx*ny*dim) = u_n([bc_y1*dim - 1]) - u_n([bc_y0*dim - 1]) - u_dif_y0(1); %x
+res([2:2:max(size(bc_y0))*dim] + nx*ny*dim) = u_n([bc_y1*dim - 0]) - u_n([bc_y0*dim - 0]) - u_dif_y0(2); %y
+res([1:2:max(size(bc_x0))*dim] + (nx*ny + max(size(bc_y0,2)) )*dim) = u_n([bc_x1*dim - 1]) - u_n([bc_x0*dim - 1]) - u_dif_x0(1); %x
+res([2:2:max(size(bc_x0))*dim] + (nx*ny + max(size(bc_y0,2)) )*dim) = u_n([bc_x1*dim - 0]) - u_n([bc_x0*dim - 0]) - u_dif_x0(2); %y
 
-res([mid_nod*dim - 1, mid_nod*dim + 0]) = u_n([mid_nod*dim - 1, mid_nod*dim + 0]) - [0.0, 0.0]';
+% fb +- µ
+res(bc_y0*dim - 1) -= u_n([1:2:max(size(bc_y0))*dim] + nx*ny*dim); %x
+res(bc_y1*dim - 1) += u_n([1:2:max(size(bc_y0))*dim] + nx*ny*dim); %x
+res(bc_y0*dim - 0) -= u_n([2:2:max(size(bc_y0))*dim] + nx*ny*dim); %y
+res(bc_y1*dim - 0) += u_n([2:2:max(size(bc_y0))*dim] + nx*ny*dim); %y
 
-jac([bc_y0*dim - 1; bc_y0*dim - 0], :) = 0.0;
-jac([bc_x0*dim - 1; bc_x0*dim - 0], :) = 0.0;
+res(bc_x0*dim - 1) -= u_n([1:2:max(size(bc_x0))*dim] + (nx*ny + max(size(bc_y0)))*dim); %x
+res(bc_x1*dim - 1) += u_n([1:2:max(size(bc_x0))*dim] + (nx*ny + max(size(bc_y0)))*dim); %x
+res(bc_x0*dim - 0) -= u_n([2:2:max(size(bc_x0))*dim] + (nx*ny + max(size(bc_y0)))*dim); %y
+res(bc_x1*dim - 0) += u_n([2:2:max(size(bc_x0))*dim] + (nx*ny + max(size(bc_y0)))*dim); %y
+
+res([X0Y0_nod*dim - 1, X0Y0_nod*dim + 0]) = u_n([X0Y0_nod*dim - 1, X0Y0_nod*dim + 0]) - [0.0, 0.0]';
 
 for n = 1 : size(bc_y0, 2)
   for d = 0 : 1
-    jac(bc_y0(n)*dim - d, bc_y1(n)*dim - d) = +1.0;
-    jac(bc_y0(n)*dim - d, bc_y0(n)*dim - d) = -1.0;
+    jac(n*dim + nx*ny*dim - d, bc_y1(n)*dim - d) = +1.0;
+    jac(n*dim + nx*ny*dim - d, bc_y0(n)*dim - d) = -1.0;
+    jac(bc_y1(n)*dim - d, n*dim + nx*ny*dim - d) = +1.0;
+    jac(bc_y0(n)*dim - d, n*dim + nx*ny*dim - d) = -1.0;
   end
 end
 
 for n = 1 : size(bc_x0, 2)
   for d = 0 : 1
-    jac(bc_x0(n)*dim - d, bc_x1(n)*dim - d) = +1.0;
-    jac(bc_x0(n)*dim - d, bc_x0(n)*dim - d) = -1.0;
+    jac(n*dim + (nx*ny + size(bc_y0,2))*dim - d, bc_x1(n)*dim - d) = +1.0;
+    jac(n*dim + (nx*ny + size(bc_y0,2))*dim - d, bc_x0(n)*dim - d) = -1.0;
+    jac(bc_x1(n)*dim - d, n*dim + (nx*ny + size(bc_y0,2))*dim - d) = +1.0;
+    jac(bc_x0(n)*dim - d, n*dim + (nx*ny + size(bc_y0,2))*dim - d) = -1.0;
   end
 end
 
-jac([mid_nod*dim - 1; mid_nod*dim - 0], :) = 0.0;
+jac([X0Y0_nod*dim - 1; X0Y0_nod*dim - 0], :) = 0.0;
 for d = 0 : 1
- jac(mid_nod*dim - d, mid_nod*dim - d) = 1.0;
+ jac(X0Y0_nod*dim - d, X0Y0_nod*dim - d) = 1.0;
 end
 
 endfunction
--- homog_bcs/run_periodic.m
+++ homog_bcs/run_periodic.m
@@ -13,10 +13,11 @@ global stress
 global strain
 global res
 
-global nx = 11;
-global ny = 11;
+global nx = 10;
+global ny = 10;
 global nelem = (nx-1)*(ny-1)
 global nnods = nx*ny;
+global size_tot
 global lx = 3;
 global ly = 3;
 global dx = lx / (nx - 1);
@@ -28,42 +29,42 @@ global nvoi = 3;
 
 init_vars();
 
+size_tot = (nx*ny + max(size(bc_y0)) + max(size(bc_x0))) * dim;
 elem_type = zeros(nelem, 1);
 
 #elements
 #coordinates
 #bc_nods
 
-du = zeros(nx*ny*dim, 1);
+du = zeros(size_tot, 1);
 strain = zeros((nx-1)*(ny-1), nvoi);
 stress = zeros((nx-1)*(ny-1), nvoi);
 
-dir_n = zeros(nx*ny*dim, 1);
 strain_exp = [0.005 0 0; 0 0.005 0; 0 0 0.005]';
 
 c_ave = zeros(3,3);
 
-%for i = 1 : 1
+for i = 1 : 3
 
-u = zeros(nx*ny*dim, 1);
-%printf ("\033[31mstrain = %f %f %f\n\033[0m", strain_exp(:,i)');
+u = zeros(size_tot, 1);
+printf ("\033[31mstrain = %f %f %f\n\033[0m", strain_exp(:,i)');
 
-[jac, res] = ass_periodic (strain_exp(:,1), u);
+[jac, res] = ass_periodic (strain_exp(:,i), u);
 printf ("\033[32m|res| = %f\n\033[0m", norm(res));
 
 du = -(jac\res);
 u = u + du;
 
-[jac, res] = ass_periodic (strain_exp(:,1), u);
+[jac, res] = ass_periodic (strain_exp(:,i), u);
 printf ("\033[32m|res| = %f\n\033[0m", norm(res));
 
 [strain_ave, stress_ave] = average()
-%c_ave(:,i) = stress_ave' / strain_ave(i);
+c_ave(:,i) = stress_ave' / strain_ave(i);
 
-%end
+end
 
-%printf ("\n");
-%c_ave
+printf ("\n");
+c_ave
 
 %figure();
 %spy(jac); print -djpg spy.jpg 
