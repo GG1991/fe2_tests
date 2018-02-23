fa06c8a665e9359247b53234884e4dea27d00e22
GG1991 <giuntoli1991@gmail.com>
2018-02-23 12:04:04 +0100

    bash script for measuring time works

--- time_vs_n.sh
+++ time_vs_n.sh
@@ -4,8 +4,20 @@ program=run_periodic_ms.m
 line_nx=18
 line_ny=19
 
-nvals=(5 10 15 20)
+#nvals=(5 10 15 20)
+nvals=(10 20 30 40 50 60 70 80 90 100)
 
-sed -e "${line_nx}s/.*/global nx = ${nvals[0]};/" -e "${line_ny}s/.*/global ny = ${nvals[0]};/" $program > program_proto.m
-octave program_proto.m > out_proto.dat
+for n in ${!nvals[@]}; do
+
+ printf "running for n = %3d  " ${nvals[$n]}
+ sed -e "${line_nx}s/.*/global nx = ${nvals[$n]};/" -e "${line_ny}s/.*/global ny = ${nvals[$n]};/" $program > program_proto.m
+ octave program_proto.m > out_proto.dat
+
+ time[$n]=$(awk '/time/{print $12;exit;}' out_proto.dat)
+ printf "time = %f\n" ${time[$n]}
+
+done
+
+echo ${time[@]}
+
 
