7c714ddb0e87ded6cdf7a2a3d5e53eab17490fc6
GG1991 <giuntoli1991@gmail.com>
2018-03-27 10:13:46 +0200

    adding plastic model in elemental.m

--- elemental.m
+++ elemental.m
@@ -23,7 +23,34 @@ if (strcmp(mat_model,"plastic"))
  
  E       = 1.0e9; 
  sig_y   = 2.0e11;
- [sig_2, eps_e_2, eps_p_2] = model_plas(eps_2, eps_e_1, eps_p_1, E, nu, sig_y)
+ d_eps   = 1.0e-6;
+
+ for gp = 1 : npe
+
+   eps_p_1 = int_vars(e*4+(gp-1), [1 2 3]);
+   eps_e_1 = int_vars(e*4+(gp-1), [4 5 6]);
+   eps_2 = b_mat(:, :, gp) * u_e;
+
+   %calc c_tan by perturbations
+   eps_2_1 = eps_2 + [d_eps; 0    ; 0    ];
+   eps_2_2 = eps_2 + [0    ; d_eps; 0    ];
+   eps_2_3 = eps_2 + [0    ; 0    ; d_eps];
+   [sig_2_1, eps_e_dummy, eps_p_dummy] = model_plas(eps_2_1, eps_e_1, eps_p_1, E, nu, sig_y);
+   [sig_2_2, eps_e_dummy, eps_p_dummy] = model_plas(eps_2_2, eps_e_1, eps_p_1, E, nu, sig_y);
+   [sig_2_3, eps_e_dummy, eps_p_dummy] = model_plas(eps_2_3, eps_e_1, eps_p_1, E, nu, sig_y);
+   c_tan(:,1) = sig_2_1/d_eps;
+   c_tan(:,2) = sig_2_2/d_eps;
+   c_tan(:,3) = sig_2_3/(2*d_eps);
+
+   %calc sig_2
+   [sig_2, eps_e_2, eps_p_2] = model_plas(eps_2, eps_e_1, eps_p_1, E, nu, sig_y);
+   int_vars(e*4+(gp-1), [1 2 3]) = eps_p_2';
+   int_vars(e*4+(gp-1), [4 5 6]) = eps_e_2';
+
+   res_e += b_mat(:, :, gp)' * sig_2 * wg(gp);
+   jac_e += b_mat(:, :, gp)' * c_tan * b_mat(:, :, gp) * wg(gp);
+
+ endfor
 
 elseif (strcmp(mat_model,"linear")) 
 
@@ -37,15 +64,13 @@ elseif (strcmp(mat_model,"linear"))
   stress(e,:) = [0.0 0.0 0.0];
   
   for gp = 1 : npe
-  
     strain_gp = b_mat(:, :, gp) * u_e;
     stress_gp = c_tan * strain_gp;
     strain(e,:) += strain_gp' * wg(gp);
     stress(e,:) += stress_gp' * wg(gp);
     res_e += b_mat(:, :, gp)' * stress_gp * wg(gp);
     jac_e += b_mat(:, :, gp)' * c_tan * b_mat(:, :, gp) * wg(gp);
-  
-  end
+  endfor
 
 endif
 
